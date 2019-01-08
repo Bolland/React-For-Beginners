d511734e7961cee602d7986f2c92939f1d4639c2
Benjamin Bolland <benjamin.bolland@gmail.com>
2019-01-08 23:26:40 +0100

    Debug commit

---
+++ catch-of-the-day/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Login = props => {
+   <nav className="login">
+      <h2>Inventory login</h2>
+   </nav>;
+};
+
+// Login.propTypes = {
+//    authenticate: PropTypes.func.isRequired
+// };
+
+export default Login;
